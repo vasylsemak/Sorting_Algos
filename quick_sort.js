0eb566be60ad9034cef37ec72e4406015e762e4b
Vasyl Semak <vasylsmk1401@gmail.com>
2021-07-10 23:50:02 -0400

    implement quick sort algo with O(1) space complexity

--- quick_sort.js
+++ quick_sort.js
@@ -1,51 +1,91 @@
+const chalk = require('chalk')
 // const myArr = [9, 7, 5, 3, 1]
 // const myArr = [99, 11, 77, 55, 22, 44]
 const myArr = [99, 95, 88, 84, 77, 74, 66, 55, 31, 22, 21, 19, 1000]
 // const myArr = Array(10000).fill(21)
-/* ----------------------------------------------------------------- */
 
 
-const chalk = require('chalk')
+/* ---------------    time O(nlogn) space O(1)     ------------------------ */
 
-function quickSort(arr) {
-  // base case for recursion
-  if(arr.length <= 1) return arr
+function partition(arr, startIdx, endIdx) {
+  const pivot = arr[endIdx - 1]
+  let pIdx = startIdx
 
-  // select first elem as pivot
-  const [pivot, ...restArr] = arr
-  const left = []
-  const right = []
+  for(let i = startIdx; i < endIdx -1; i++) {
+    let curElem = arr[i]
 
-  // iterate through arr, push all smaller nums to leftArr; greater - to right
-  restArr.forEach(el => el < pivot ? left.push(el) : right.push(el))
+    if(curElem < pivot) {
+      arr[i] = arr[pIdx]
+      arr[pIdx] = curElem
+      pIdx++
+    }
+  }
+  arr[endIdx - 1] = arr[pIdx]
+  arr[pIdx] = pivot
 
-  // run f-n recursively on left and right arrays
-  const leftSorted= quickSort(left)
-  const rightSorted = quickSort(right)
-
-  // return leftSortedArr + pivot + rightSortedArr
-  return leftSorted.concat(pivot).concat(rightSorted)
+  // console.log('pIdx -> ', pIdx)
+  // console.log('After Switch -> ', arr)
+  return pIdx
 }
 
 
-console.log(
-  chalk.yellow('sorted -> '),
-  chalk.red(quickSort(myArr))
-)
+
+function quickSort(arr, start = 0, end = arr.length) {
+  if(start < end) {
+    let pivotIdx = partition(arr, start, end)
+
+    // edge case to avoid extra iterations
+    if((pivotIdx+1) < (end - 1)) quickSort(arr, pivotIdx + 1, end)
+    quickSort(arr, 0, pivotIdx)
+  }
+
+  return arr
+}
 
 
 
-// /* ------        Perfomance F-N              --------------------- */
-// function getPerfomance(func, arr) {
-//   const start = Date.now()
-//   const result = func(arr)
-//   const end = Date.now()
-//   let perfomance = (end - start) / 100
-//   console.log('PERFOMANCE ===> ', perfomance, ' seconds.')
-//   return result
+/* ---------------    time O(nlogn) space O(n)     ------------------------ */
+
+// function quickSort(arr) {
+//   // base case for recursion
+//   if(arr.length <= 1) return arr
+
+//   // select first elem as pivot
+//   const [pivot, ...restArr] = arr
+//   const left = []
+//   const right = []
+
+//   // iterate through arr, push all smaller nums to leftArr; greater - to right
+//   restArr.forEach(el => el < pivot ? left.push(el) : right.push(el))
+
+//   // run f-n recursively on left and right arrays
+//   const leftSorted= quickSort(left)
+//   const rightSorted = quickSort(right)
+
+//   // return leftSortedArr + pivot + rightSortedArr
+//   return leftSorted.concat(pivot).concat(rightSorted)
 // }
+
+
 
 // console.log(
 //   chalk.yellow('sorted -> '),
-//   chalk.red(getPerfomance(quickSort, myArr))
+//   chalk.red(quickSort(myArr))
 // )
+
+
+
+/* ------        Perfomance F-N              --------------------- */
+function getPerfomance(func, arr) {
+  const start = Date.now()
+  const result = func(arr)
+  const end = Date.now()
+  let perfomance = (end - start) / 100
+  console.log('PERFOMANCE ===> ', perfomance, ' seconds.')
+  return result
+}
+
+console.log(
+  chalk.yellow('sorted -> '),
+  chalk.red(getPerfomance(quickSort, myArr))
+)
